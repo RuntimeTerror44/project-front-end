9f9c0445004cb8af4ddfb533fac4e9c3f5f73c3e
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-05-18 10:23:36 +0000

    create react app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(<App />, document.getElementById('root'));
