ea95b533574c5058aed9a48df8874948130139c8
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-05-20 09:18:44 +0000

    Login form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { BrowserRouter } from 'react-router-dom';
-const domain=process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId=process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 ReactDOM.render(
-
-
 
 <App />
 
