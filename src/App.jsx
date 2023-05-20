36cb6bdc0e7dc63920eab0f348228e711d3e96de
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-05-20 09:03:55 +0000

    edit the main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
 // import { Route,Routes } from 'react-router-dom';
-import Profile from './components/Profile';
 export default function App() {
   return (
    <>
-   <h1>Auth0 Login</h1>
-   <LoginButton/>
-   <LogoutButton/>
-   <Profile/>
+   <h1>hello</h1>
+  
    
    
    
