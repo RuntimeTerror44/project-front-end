24eaab278d4db6dca651de632725b2005feedeaa
Mohammad-Aljamal <mohammad.o.aljamal@gmail.com>
2023-05-21 19:25:34 +0000

    adding user form and conect with backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import FormPersonalData from "./components/Profile/FormPersonalData";
-import LoginButton from './components/Landingpage/login/Loginbutton';
-import LogoutButton from './components/Landingpage/login/Logoutbutton';
+import React from "react";
+// import LandingPage from "./components/Landingpage/LandingPage";
+// import LoginButton from "./components/Landingpage/login/Loginbutton";
+// import LogoutButton from "./components/Landingpage/login/Logoutbutton";
 // import { Route,Routes } from 'react-router-dom';
-import Profile from './components/Landingpage/login/profile';
+import FormPersonalData from "./components/Profile/FormPersonalData";
+// import Profile from "./components/Landingpage/login/profile";
 export default function App() {
   return (
-    
-   <>
-   <FormPersonalData />  {/* form to get the user data */}
-
-
-   <h1>Auth0 Login</h1>
-   <LoginButton/>
-   <LogoutButton/>
-   <Profile/>
-
-
-   </>
-  )
+    <>
+      {/* <LandingPage/> */}
+      <FormPersonalData /> {/* form to get the user data */}
+      {/* <h1>Auth0 Login</h1>
+      <LoginButton />
+      <LogoutButton />
+      <Profile /> */}
+    </>
+  );
 }
