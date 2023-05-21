f4891396f76b12fb82af0d32bf4a923372fa4280
esraaobeido <esraaabeido123@gmail.com>
2023-05-21 19:25:37 +0000

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import React from 'react';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
-import Login from './components/LandingPage/Login/Login';
-import Signup from './components/LandingPage/Login/Signup';
+import React from "react";
+import LandingPage from "./components/Landingpage/LandingPage";
+import FormPersonalData from "./components/Profile/FormPersonalData";
+import LoginButton from "./components/Landingpage/login/Loginbutton";
+import LogoutButton from "./components/Landingpage/login/Logoutbutton";
+import Home from "./components/Home/Home"
+// import { Route,Routes } from 'react-router-dom';
+import Profile from "./components/Landingpage/login/profile";
+
 export default function App() {
   return (
-   <>
-  <BrowserRouter>
-  <Routes>
-  <Route path='/' element={<Login/>}/>
-  <Route path='/signup' element={<Signup/>}/>
-
-
-  </Routes>
-  
-   
-  </BrowserRouter>
-   
-   
-   </>
-  )
+    <>
+    <Home/>
+    </>
+  );
 }
---
+++ src/components/Landingpage/login/profile.js
@@ -0,0 +1,31 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./Loginbutton";
+import LogoutButton from "./Logoutbutton";
+import { useState } from "react";
+import axios from "axios";
+import Test from "./Test";
+
+const Profile =(savedData)=>{
+    
+const {user,isAuthenticated}= useAuth0();
+
+return (
+isAuthenticated &&(
+
+
+
+<>
+
+
+{/* <p>{user.nickname}</p>
+<img src={user.picture}></img>
+<p>{user.email}</p>
+<p>{user.family_name}</p> */}
+<Test data1={user.given_name} data2={user.picture} data3={user.email}/>
+
+
+
+</>
+))}
+
+export default Profile
