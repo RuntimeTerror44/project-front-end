e2c0a2393eb990ce648fd440a8a0f60442a5c222
esraaobeido <esraaabeido123@gmail.com>
2023-05-24 16:28:30 +0000

    store comments indb

--- src/components/Home/Comment.js
+++ src/components/Home/Comment.js
@@ -1,71 +1,84 @@
-// import { Navbar, Nav, Container, Form, FormControl, Button, Row, Col, Card, Dropdown } from 'react-bootstrap';
+import { Navbar, Nav, Container, Form, FormControl, Button, Row, Col, Card, Dropdown } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useRef } from "react";
 
-// function Comment(props){
-//     // const handleAddComment = (postId, commentText) => {
-//     //     const newComment = {
-//     //       id: Date.now(),
-//     //       text: commentText,
-//     //     };
+
+
+function Comment(props){
+    const [commentsDataArray, setCommentsDataArray] = useState([]);
+
+    const getCommentsFromDb = async () => {
+      const serverUrl = `${process.env.REACT_APP_SERVER_URL}comments/52`; 
+      const result = await axios.get(serverUrl);
+        setCommentsDataArray(result.data)
+    //  console.log(commentsDataArray);
+      }
     
-//     //     setPosts((prevPosts) => {
-//     //       const updatedPosts = prevPosts.map((post) => {
-//     //         if (post.id === postId) {
-//     //           return {
-//     //             ...post,
-//     //             comments: [...post.comments, newComment],
-//     //           };
-//     //         }
-//     //         return post;
-//     //       });
-//     //       return updatedPosts;
-//     //     });
-//     //   };
-//     return(
-//         <div>
-            
-        
-//             <div className="posts-container">
-//               {props.posts.map((post) => (
-//                 <Card key={post.id} className="post">
-//                   <Card.Body>
-//                     <Card.Text>{post.text}</Card.Text>
-//                     <div className="comments-container">
-//                       {post.comments.map((comment) => (
-//                         <Card.Text key={comment.id} className="comment">
-//                           {comment.text}
-//                         </Card.Text>
-//                       ))}
-//                     </div>
+    // console.log(commentsDataArray);
 
 
-//                     <Form
-//                       onSubmit={(event) => {
-//                         event.preventDefault();
-//                         handleAddComment(post.id, event.target.comment.value);
-//                         event.target.comment.value = '';
-//                       }}
-//                     >
-//                       <Form.Group controlId={`commentForm-${post.id}`}>
-//                         <Form.Control type="text" name="comment" placeholder="Add a comment"/>
-//                       </Form.Group>
-//                       <Button variant="primary" type="submit">
-//                         Add Comment
-//                       </Button>
-//                     </Form>
-//                     <Dropdown align="end">
-//                     <Dropdown.Toggle variant="primary" id={`dropdown-${post.id}`} className="dropdown-toggle-vertical">
-//                      Options
-//                     </Dropdown.Toggle>
-//                       <Dropdown.Menu>
-//                         <Dropdown.Item onClick={() => handleEditPost(post.id)}>Edit</Dropdown.Item>
-//                         <Dropdown.Item onClick={() => handleDeletePost(post.id)}>Delete</Dropdown.Item>
-//                       </Dropdown.Menu>
-//                     </Dropdown>
-//                   </Card.Body>
-//                 </Card>
-//               ))}
-//             </div>
-//         </div>
-//     )
-// }
-// export default Comment;
+    useEffect(() => {
+      getCommentsFromDb();
+      console.log(commentsDataArray);
+    },[]);
+
+    useEffect(() => {
+      },[commentsDataArray]);
+    
+    const handleAddComment = async (e) => {
+        try {
+        e.preventDefault();
+          const serverUrl = `${process.env.REACT_APP_SERVER_URL}comments/52`;
+          const obj = {
+            user_id: 2,
+            content: e.target.comment.value
+          };
+          const response = await axios.post(serverUrl, obj);
+          console.log(response.data);
+
+          setCommentsDataArray(response.data);
+        //   console.log(commentsDataArray);
+        } catch (error) {
+          console.log('Error adding comment', error);
+        }
+      };
+      return (
+        <div>
+          <Form
+                onSubmit= {handleAddComment}>
+                    <Form.Group >
+                    <Form.Control
+                      type="text"
+                      name="comment"
+                      placeholder="Add a comment"/>
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Add a Comment
+                  </Button>
+                  </Form>
+                  {/* {console.log(commentsDataArray)} */}
+                {commentsDataArray.map((item) => {  
+                    const text = (<Row xs={1} md={4} className="g-4">   
+                    <Col>
+                      <Card >
+                        <Card.Img variant="top" />
+                        <Card.Body>
+                          <Card.Text>
+                           <p>knjnjbhbhb</p>
+                           <p>{item.content}</p>
+                           <p>{item.comment_date}</p>
+                           <p>{item.user_id}</p>
+                           <p>{item.post_id}</p>
+                          </Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                </Row>)  
+                return text;            
+              })}
+              
+        </div>
+      );
+    };
+    export default Comment;
