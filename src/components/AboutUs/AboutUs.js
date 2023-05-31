b8ff50fb05fe3fed14a4f51b096a13417e8c993f
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2023-05-31 05:56:26 +0000

    cleaning css files

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -5,6 +5,14 @@ import { TweenMax } from "gsap";
 import "typicons.font";
 import { color } from "framer-motion";
 
+import React from "react";
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+} from "mdb-react-ui-kit";
 
 
 function AboutUs() {
@@ -325,6 +333,84 @@ function AboutUs() {
         </div>
       </section>          
     </body>
+
+    <MDBFooter
+        bgColor="light"
+        className="text-center text-lg-start text-muted"
+      >
+        <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+          <div className="me-5 d-none d-lg-block">
+            <span>Get connected with us on social networks:</span>
+          </div>
+          <div>
+            <a href="" className="me-4 text-reset">
+              <MDBIcon fab icon="google" />
+            </a>
+            <a href="" className="me-4 text-reset">
+              <MDBIcon fab icon="linkedin" />
+            </a>
+            <a href="" className="me-4 text-reset">
+              <MDBIcon fab icon="github" />
+            </a>
+          </div>
+        </section>
+        <section className="">
+          <MDBContainer className="text-center text-md-start mt-5">
+            <MDBRow className="mt-3">
+              <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">
+                  <MDBIcon icon="gem" className="me-3" />
+                  Career Connect
+                </h6>
+              </MDBCol>
+              <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+                <p>
+                  <a href="#!" className="text-reset">
+                    privasy statement
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Settings
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    pricing
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Help
+                  </a>
+                </p>
+              </MDBCol>
+              <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+                <p>
+                  <MDBIcon icon="home" className="me-2" />
+                  Amman , jordan
+                </p>
+                <p>
+                  <MDBIcon icon="envelope" className="me-3" />
+                  careers@gmail.com
+                </p>
+                <p>
+                  <MDBIcon icon="phone" className="me-3" /> 0796658743
+                </p>
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+        </section>
+        <div
+          className="text-center p-4"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+        >
+          © 2023 Copyright:
+          <p>CareerConnect</p>
+        </div>
+      </MDBFooter>
     </>
   );
 }
